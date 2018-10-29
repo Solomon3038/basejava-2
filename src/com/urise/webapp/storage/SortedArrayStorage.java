e6cc47921d94ed36e1ae8d3e43afbc4511119f88
Kalinov_PM <kalinov.pavel11@gmail.com>
2018-10-28 18:33:11 -1200

    Added classes: MapUuidStorage, MapUuidStorageTest, AbstractArrayStorageTest, AllStorageTest. Refactoring.

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -5,6 +5,8 @@ import com.urise.webapp.model.Resume;
 import java.util.Arrays;
 import java.util.Comparator;
 
+import static java.util.Comparator.*;
+
 public class SortedArrayStorage extends AbstractArrayStorage {
 
     /*private static class ResumeComparator implements Comparator<Resume>{
@@ -14,13 +16,7 @@ public class SortedArrayStorage extends AbstractArrayStorage {
         }
     }*/
 
-    private static final Comparator<Resume> RESUME_COMPARATOR = new Comparator<Resume>(){
-
-        @Override
-        public int compare(Resume o1, Resume o2) {
-            return o1.getUuid().compareTo(o2.getUuid());
-        }
-    };
+    private static final Comparator<Resume> RESUME_COMPARATOR = comparing(Resume::getUuid);
 
     @Override
     protected void fillDeletedElement(int index) {
@@ -40,7 +36,7 @@ public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     protected Integer getSearchKey(String uuid) {
-        Resume searchKey = new Resume(uuid);
+        Resume searchKey = new Resume(uuid, "dummy");
         return Arrays.binarySearch(storage, 0, size, searchKey, RESUME_COMPARATOR);
     }
 }
---
+++ test/com/urise/webapp/storage/MapUuidStorageTest.java
@@ -0,0 +1,7 @@
+package com.urise.webapp.storage;
+
+public class MapUuidStorageTest extends AbstractStorageTest {
+    protected MapUuidStorageTest() {
+        super(new MapUuidStorage());
+    }
+}
