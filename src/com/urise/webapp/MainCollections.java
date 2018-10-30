ed0dfaa18c0764156794967fb0f72c5af8fa9e9c
Kalinov_PM <p.m.kalinov@onegroup.ru>
2018-10-30 17:16:57 +0300

    Add generics.

--- src/com/urise/webapp/MainCollections.java
+++ src/com/urise/webapp/MainCollections.java
@@ -56,6 +56,7 @@ public class MainCollections {
         }
 
         List<Resume> resumes = Arrays.asList(RESUME_1, RESUME_2, RESUME_3);
+
         resumes.remove(1); // java.lang.UnsupportedOperationException
         System.out.println(resumes);
     }
